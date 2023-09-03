3ef00e2743331984f97606ca170b660549114ee0
gosha <gosha.titov@icloud.com>
2023-09-03 18:00:10 +0300

    Docs: update descriptions

--- Sources/Theme.swift
+++ Sources/Theme.swift
@@ -1,16 +1,19 @@
 /// A theme that provides appearance that UI elements adhere to.
 ///
 /// A theme is a collection of UI attributes that form the appearance of the application.
-/// They are commonly expressed with such attributes as:
+/// It's commonly expressed with such attributes as:
 /// - Colors: background colors, text colors, button colors, etc;
-/// - Layout: width, height, padding, margin, alignment, etc;
+/// - Layout: widths, heights, paddings, margins, alignments, etc;
 /// - Typography: fonts and their sizes;
-/// - Icons: button images, etc;
+/// - Images: icons, wallpapers, etc.
 ///
 /// These attributes can be grouped into categories.
 public protocol TFTheme: AnyObject {
     
-    /// A string value associated with this theme.
+    /// A string associated with the name of this theme.
+    ///
+    /// You usually name themes as follows: `Light`, `Dark`, `Sunrise`, `Sunset` or `Custom`.
+    /// It's used, for example, when you need to display a list of available themes; or for debugging / logging.
     var name: String { get }
     
 }
