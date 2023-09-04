80513a419b4f4092b138bf8cc0de4106de05a79b
gosha <gosha.titov@icloud.com>
2023-09-04 12:19:46 +0300

    Feat: add save, load and remove methods

--- Sources/Strorage.swift
+++ Sources/Strorage.swift
@@ -1,13 +1,47 @@
+import Foundation
+
 /// A storage that is responsible for managing the current theme in `UserDefaults`.
 internal final class TFStorage {
     
+    // MARK: - Properties
+    
     /// The singleton storage instance.
     internal let shared = TFStorage()
+    
+    /// The shared defaults objects.
+    private let defaults = UserDefaults.standard
+    
+    
+    // MARK: - Methods
+    
+    /// Saves the given name in `UserDefaults` as the name of the current theme.
+    internal final func saveNameOfCurrentTheme(_ themeName: String) -> Void {
+        defaults.set(themeName, forKey: AssociatedKeys.themeName)
+    }
+    
+    /// Loads the name of the current theme from `UserDefaults`.
+    internal final func loadNameOfCurrentTheme() -> String? {
+        return defaults.string(forKey: AssociatedKeys.themeName)
+    }
+    
+    /// Removes the name of the current theme from `UserDefaults`.
+    internal final func removeNameOfCurrentTheme() -> Void {
+        defaults.removeObject(forKey: AssociatedKeys.themeName)
+    }
     
     
     // MARK: - Init
     
     /// Creates a storage instance.
     private init() {}
+    
+}
+
+
+extension TFStorage {
+    
+    private enum AssociatedKeys {
+        static let themeName = "com.themeful-current_theme_name"
+    }
     
 }
