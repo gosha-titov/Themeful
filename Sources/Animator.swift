111605b0336bbb45c62ab3be1fb9105e0d6db938
gosha <gosha.titov@icloud.com>
2023-09-04 16:25:14 +0300

    Feat: add the `updateAppearance(of:)` method

--- Sources/Animator.swift
+++ Sources/Animator.swift
@@ -1,13 +1,39 @@
+import UIKit
+
 /// An animator that is responsible for animating appearance updates.
 internal final class TFAnimator {
     
     /// The singleton animator instance.
     internal static let shared = TFAnimator()
     
+    /// The total duration of the animations of appearance updates, measured in seconds.
+    internal var animationDuration: CGFloat = 0.3
+    
+    /// A boolean value that indicates whether the appearance updates are animated.
+    internal var animatesAppearanceUpdates: Bool = true
+    
+    /// Update appearance of the given object, animated.
+    internal final func updateAppearance(of object: any Object, with theme: TFTheme) -> Void {
+        if animatesAppearanceUpdates, object.prefersAppearanceUpdateToBeAnimated {
+            UIView.animate(withDuration: animationDuration) {
+                object.internalUpdateAppearance(with: theme)
+            }
+        } else {
+            object.internalUpdateAppearance(with: theme)
+        }
+    }
+    
     
     // MARK: - Init
     
     /// Creates an animator instance.
     private init() {}
+    
+}
+
+
+extension TFAnimator {
+    
+    internal typealias Object = TFAppearanceUpdatable
     
 }
