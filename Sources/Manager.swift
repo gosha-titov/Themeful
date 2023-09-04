466801d435204511699b042ce82757ada80ec374
gosha <gosha.titov@icloud.com>
2023-09-04 17:02:07 +0300

    Feat: add `loadCurrentTheme()` and `theme(by:)` methods

--- Sources/Manager.swift
+++ Sources/Manager.swift
@@ -1,11 +1,47 @@
 /// A manager that is responsible for managing themes and notifying subscribers about it.
-open class TFManager<Theme: TFTheme> {
+open class TFManager<Theme> where Theme: TFTheme {
     
-    /// The current theme of the application.
+    // MARK: - Properties
+    
+    /// The current theme of the application, settable.
+    ///
+    /// When you set a new value for this property, it automatically updates appearances of subscribed objects,
+    /// and saves the name of this new theme to `UserDefaults` as the current one.
     public final var currentTheme: Theme {
-        didSet {
-            // notifying
+        willSet (newTheme) {
+            TFPublisher.shared.updateCurrentTheme(with: newTheme)
+            TFStorage.shared.saveNameOfCurrentTheme(newTheme.name)
         }
+    }
+    
+    
+    // MARK: - Methods
+    
+    /// Returns the current theme loaded from `UserDefaults`.
+    public final func loadCurrentTheme() -> Theme? {
+        guard let themeName = TFStorage.shared.loadNameOfCurrentTheme(),
+              let theme = theme(by: themeName)
+        else { return nil }
+        return theme
+    }
+    
+    /// Returns a specific theme associated with the given name.
+    ///
+    /// You should override this method in the following way:
+    ///
+    ///     override func theme(by name: String) -> Theme? {
+    ///         switch name {
+    ///         case "Light": return LightTheme()
+    ///         case "Dark":  return DarkTheme()
+    ///         case "Ocean": return OceanTheme()
+    ///         default: return nil
+    ///         }
+    ///     }
+    ///
+    /// You don't need to call the `super` method.
+    /// - Returns: The associated theme; otherwise, `nil`.
+    open func theme(by name: String) -> Theme? {
+        return nil
     }
     
     
