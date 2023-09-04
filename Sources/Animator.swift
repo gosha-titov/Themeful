a2c44ab2faca89a84f0446945633708ef1fa5b3a
gosha <gosha.titov@icloud.com>
2023-09-04 14:56:55 +0300

    Feat: define the `TFAnimator` class

---
+++ Sources/Animator.swift
@@ -0,0 +1,13 @@
+/// An animator that is responsible for animating appearance updates.
+internal final class TFAnimator {
+    
+    /// The singleton animator instance.
+    internal static let shared = TFAnimator()
+    
+    
+    // MARK: - Init
+    
+    /// Creates an animator instance.
+    private init() {}
+    
+}
