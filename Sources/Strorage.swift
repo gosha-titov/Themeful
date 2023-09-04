edcfa6938bdc80703882ec670f02b3f183aa5ab9
gosha <gosha.titov@icloud.com>
2023-09-04 11:21:16 +0300

    Feat: define the `TFStorage` class

---
+++ Sources/Strorage.swift
@@ -0,0 +1,13 @@
+/// A storage that is responsible for managing the current theme in `UserDefaults`.
+internal final class TFStorage {
+    
+    /// The singleton storage instance.
+    internal let shared = TFStorage()
+    
+    
+    // MARK: - Init
+    
+    /// Creates a storage instance.
+    private init() {}
+    
+}
