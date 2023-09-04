3aa6b6aecf9442861244f091926152dc0fc53e8a
gosha <gosha.titov@icloud.com>
2023-09-04 12:22:20 +0300

    Refactor: update the access-level

--- Sources/Publisher.swift
+++ Sources/Publisher.swift
@@ -1,6 +1,8 @@
 /// A publisher that is responsible for notifying about updates of the objects' appearance.
 internal final class TFPublisher {
     
+    // MARK: - Properties
+    
     /// The singleton publisher instance.
     internal static let shared = TFPublisher()
     
@@ -16,7 +18,7 @@ internal final class TFPublisher {
     private var lastID: ObjectID = 0
     
     
-    // MARK: - Object Managing Methods
+    // MARK: - Registering and Releasing Methods
     
     /// Adds the given object to the tracked ones.
     /// - Returns: An ID associated with this object.
@@ -25,6 +27,44 @@ internal final class TFPublisher {
         let objectID = findAvailableID()
         trackedObjects[objectID] = weakObject
         object.objectID = objectID
+    }
+    
+    /// Removes the given object from tracked ones.
+    internal final func releaseFromAppearanceUpdates(_ object: any Object) -> Void {
+        let ids = trackedObjects.filter { $0.value.reference === object }.keys
+        for id in ids { releaseFromAppearanceUpdates(by: id) }
+    }
+    
+    /// Removes a specific object from tracked ones by the given ID.
+    internal final func releaseFromAppearanceUpdates(by id: ObjectID) -> Void {
+        if let object = trackedObjects.removeValue(forKey: id)?.reference {
+            object.objectID = nil
+        }
+    }
+    
+    
+    // MARK: Updating Appearance Methods
+    
+    /// Updates appearances of all tracked objects by passing the current theme to them.
+    internal func updateAppearanceOfAllObjects() -> Void {
+        removeNilObjects()
+        let objects = trackedObjects.compactMap { $0.value.reference }
+        for object in objects {
+            updateAppearance(of: object)
+        }
+    }
+    
+    /// Updates an appearance of the given object by passing the current theme to it.
+    internal final func updateAppearance(of object: any Object) -> Void {
+        object.internalUpdateAppearance(with: currentTheme)
+    }
+    
+    
+    // MARK: Other Methods
+    
+    /// Removes tracked objects that have `nil` references.
+    private func removeNilObjects() -> Void {
+        trackedObjects = trackedObjects.filter { $0.value.reference != nil }
     }
     
     /// Finds the nearest available ID, recursively.
@@ -40,36 +80,6 @@ internal final class TFPublisher {
         } else {
             return lastID
         }
-    }
-    
-    /// Removes the given object from tracked ones.
-    internal final func releaseFromAppearanceUpdates(_ object: any Object) -> Void {
-        let ids = trackedObjects.filter { $0.value.reference === object }.keys
-        for id in ids { releaseFromAppearanceUpdates(by: id) }
-    }
-    
-    /// Removes a specific object from tracked ones by the given ID.
-    internal final func releaseFromAppearanceUpdates(by id: ObjectID) -> Void {
-        trackedObjects.removeValue(forKey: id)
-    }
-    
-    /// Updates appearances of all tracked objects by passing the current theme to them.
-    private func updateAppearanceOfAllObjects() -> Void {
-        removeNilObjects()
-        let objects = trackedObjects.compactMap { $0.value.reference }
-        for object in objects {
-            updateAppearance(of: object)
-        }
-    }
-    
-    /// Updates an appearance of the given object by passing the current theme to it.
-    internal final func updateAppearance(of object: any Object) -> Void {
-        object.internalUpdateAppearance(with: currentTheme)
-    }
-    
-    /// Removes tracked objects that have `nil` references.
-    private func removeNilObjects() -> Void {
-        trackedObjects = trackedObjects.filter { $0.value.reference != nil }
     }
     
     
